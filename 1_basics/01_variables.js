12eb483c6e775d6e19359dc5b208dbf9d79fd771
rajeevrathore3088 <130215181+rajeevrathore3088@users.noreply.github.com>
2024-04-01 08:45:46 +0000

    datatypes

---
+++ 1_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 22321
+let accountEmail = "rajeev @gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+// let accountState=
+
+// accountID = 2 not allowed
+// for decclaring constant u have to write const keyword
+// use only const and let
+// not to use var bcoz of issue in block scope 
+
+console.log(accountID);
+// table is used for table making
+console.table([accountEmail,accountID,accountPassword])
---
+++ 1_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; 
+
+// alert we are using node js not a browser
+
+console.log(3+3);
+
+
+let name = "ujjj"
+let isLoggedIn = false
+
+alll are the datatypes
+// BigInt
+// String=> ""
+// Boolean => true/false
+// null =>standalone value
+// Symbol =>unique
+
+
+
+// Object
+
+console.log(typeof undefined);
