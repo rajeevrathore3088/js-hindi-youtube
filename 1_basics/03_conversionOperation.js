aacb0235de9be9f801d3062e7eefaebc72ca5d1e
rajeevrathore3088 <130215181+rajeevrathore3088@users.noreply.github.com>
2024-04-02 18:34:35 +0000

    conversion comparison

--- 1_basics/03_conversionOperation.js
+++ 1_basics/03_conversionOperation.js
@@ -2,12 +2,12 @@ let score = "44"
 
 
 // it is used to tell the type of datatypes
-console.log(typeof score );
-console.log(typeof(score));
+// console.log(typeof score );
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 
@@ -16,9 +16,42 @@ console.log(valueInNumber);
 // true =>1; false=>0
 
 
-let isLoggedIn = ""
-let BooleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(BooleanIsLoggedIn);
+// let isLoggedIn = ""
+// let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanIsLoggedIn);
 
 // 1 in boolean converts to true
 // empty string is false ("")
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "hello"
+let str2 ="hiiii"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" +2);
+// console.log(1+2+"2");
+// console.log("1"+2+2);
+
+// console.log(+true);
+
+// here is the increement of the value 
+let game = 100
+game++;
+console.log(game);
+
+
+
+// postfix increment
+// let x = 3;
+// const y = x++;
+// x is 4; y is 3
+  
+
+// prefix increment
+// let x = 3;
+// const y = ++x;
+// x is 4; y is 4
