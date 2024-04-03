b83b91daccc38da1bd14f061d8881055766f5f75
rajeevrathore3088 <130215181+rajeevrathore3088@users.noreply.github.com>
2024-04-03 09:10:08 +0000

    datatypes summary

---
+++ 1_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// primitive
+
+// 7 types : string,number,null,undefined,boolean,symbol,BigInt
+
+// Reference (Non primitive)
+
+const score = 100
+const scoreValue = 100.4
+
+const isLoggedIn = false
+const outsideTemp = null
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 21313123232323n
+
+
+// const heros = ["shaktiman","dakuuu"]
+// let myObj ={
+//     name:"rajev"
+// }
+
+
+// function name(params) 
+const myFunction = function(){
+console.log("hello ");
+}
+
+console.log(typeof scoreValue);
+
+
+
+// Array,Objects,functions
