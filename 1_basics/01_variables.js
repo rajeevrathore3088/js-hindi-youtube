46b2515e46bc71603452fb65ea5c3975e4d4322d
rajeevrathore3088 <130215181+rajeevrathore3088@users.noreply.github.com>
2024-04-07 23:15:07 +0530

    Update 01_variables.js

--- 1_basics/01_variables.js
+++ 1_basics/01_variables.js
@@ -1,14 +1,14 @@
+// These are the some variables in javascript
+// 1(var)
+// 1(let)
+// 3(const)
 const accountID = 22321
 let accountEmail = "rajeev @gmail.com"
 var accountPassword = "1234"
 accountCity = "jaipur"
-// let accountState=
-
-// accountID = 2 not allowed
-// for decclaring constant u have to write const keyword
+// the constant keyword cannot be changed once it would written
 // use only const and let
 // not to use var bcoz of issue in block scope 
 
 console.log(accountID);
-// table is used for table making
 console.table([accountEmail,accountID,accountPassword])
