db39278ca58e2f8aa6030a317645d4e16684110c
rajeevrathore3088 <130215181+rajeevrathore3088@users.noreply.github.com>
2024-04-09 17:22:55 +0000

    dates

---
+++ 1_basics/07_datesinJs.js
@@ -0,0 +1,42 @@
+//  dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString);
+
+
+// let myCreteDate = new Date(2020, 0, 20) here it show the date
+// let myCreteDate = new Date(2020, 0, 20, 5, 4) here it show the time
+// let myCreteDate = new Date("2020-02-13") (yyyy/mm/dd)
+let myCreteDate = new Date("04-30-2033") 
+// console.log(myCreteDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+weekday:"long",
+// timeZone
+
+})
+console.log(newDate);
+
+
+
+
+
+
+
+
+
+
+
+
